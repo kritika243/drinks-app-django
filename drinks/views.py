75815cca84be25a03507d7b7075654bc67b880ab
kritika243 <24kritikasharma24@gmail.com>
2023-01-31 11:40:03 +0530

    defining a url for endpoint, returning json object instead of list

--- drinks/views.py
+++ drinks/views.py
@@ -10,4 +10,4 @@ def drink_list(request):
 
   drinks = Drink.objects.all()
   serializer = DrinkSerializers(drinks, many = True)
-  return JsonResponse(serializer.data)
+  return JsonResponse({"drinks":serializer.data}, safe = False)
