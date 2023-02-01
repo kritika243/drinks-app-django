bd4528b446a3642326412117ba2ed07a0266ea84
kritika243 <24kritikasharma24@gmail.com>
2023-02-01 23:10:31 +0530

    post request and using decorator

--- drinks/views.py
+++ drinks/views.py
@@ -1,13 +1,23 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET','POST'])
 def drink_list(request):
 
   # get all the drinks 
   # serialize them
   # return json 
+  if request.method == 'GET':
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializers(drinks, many = True)
+    return JsonResponse({"drinks":serializer.data})
 
-  drinks = Drink.objects.all()
-  serializer = DrinkSerializers(drinks, many = True)
-  return JsonResponse({"drinks":serializer.data})
+  if request.method == 'POST':
+    serializer = DrinkSerializers(data = request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status = status.HTTP_201_CREATED)
